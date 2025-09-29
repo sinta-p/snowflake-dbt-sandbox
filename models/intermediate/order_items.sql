ad5dc5f66b46201df0092d8d78a4f9688722dc85
Sin Ta Poon <sinta.poon@dbtlabs.com>
2025-09-29 09:24:11 +0000

    add order_items

---
+++ models/intermediate/order_items.sql
@@ -0,0 +1,60 @@
+with line_items as (
+    select 
+        orderkey,
+        partkey,
+        suppkey,
+        linenumber,
+        quantity,
+        extendedprice,
+        discount,
+        tax,
+        returnflag,
+        linestatus,
+        shipdate,
+        commitdate,
+        receiptdate,
+        shipinstruct,
+        shipmode,
+        comment
+    from {{ ref('stg_tchp_line_items') }}
+),
+orders as (
+    select 
+        orderkey,
+        custkey,
+        orderstatus,
+        totalprice,
+        orderdate,
+        orderpriority,
+        clerk,
+        shippriority,
+        comment
+    from {{ ref("stg_tpch_orders") }}
+)
+select 
+    li.orderkey,
+    li.partkey,
+    li.suppkey,
+    li.linenumber,
+    li.quantity,
+    li.extendedprice,
+    li.discount,
+    li.tax,
+    li.returnflag,
+    li.linestatus,
+    li.shipdate,
+    li.commitdate,
+    li.receiptdate,
+    li.shipinstruct,
+    li.shipmode,
+    li.comment as lineitem_comment,
+    o.custkey,
+    o.orderstatus,
+    o.totalprice,
+    o.orderdate,
+    o.orderpriority,
+    o.clerk,
+    o.shippriority,
+    o.comment as order_comment
+from line_items li
+inner join orders o on li.orderkey = o.orderkey
--- models/staging/stg_tpch_orders.sql
+++ models/staging/stg_tpch_orders.sql
@@ -1,1 +1,11 @@
-select * from {{source("tpch_now","orders")}}
+select 
+    O_ORDERKEY as orderkey,
+    O_CUSTKEY as custkey,
+    O_ORDERSTATUS as orderstatus,
+    O_TOTALPRICE as totalprice,
+    O_ORDERDATE as orderdate,
+    O_ORDERPRIORITY as orderpriority,
+    O_CLERK as clerk,
+    O_SHIPPRIORITY as shippriority,
+    O_COMMENT as comment
+from {{ source("tpch_now", "orders") }}
